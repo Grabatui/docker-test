854c214d39f5b6ca89ced92fc58ba48421ed9a88
Grabatui <anatoliy.bibanaev@gmail.com>
2021-04-18 11:53:50 +0500

    9. Env variables

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT;
+
 app.get('/test', (request, response) => {
     response.send('It\'s working!');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Started');
 });
