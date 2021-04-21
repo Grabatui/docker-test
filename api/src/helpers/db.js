f135c75c426da317cbe4ca3cab4ee2c6702c33b5
Grabatui <anatoliy.bibanaev@gmail.com>
2021-04-21 23:52:12 +0500

    12. Volumes

--- api/src/helpers/db.js
+++ api/src/helpers/db.js
@@ -4,6 +4,7 @@ const { db } = require('../configuration');
 module.exports.connectToDatabase = () => {
     mongoose.connect(db, {
         useNewUrlParser: true,
+        useUnifiedTopology: true,
     });
 
     return mongoose.connection;
